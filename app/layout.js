494ab3d0a0b3bacbd529063165d3f0bc3436e4ef
Reeturaj Kumar <reeturajvats587@gmail.com>
2025-03-31 12:20:29 +0530

    designing HomePage

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,13 @@
+
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from './../components/header';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  subsets: ["latin"], 
 });
 
 const geistMono = Geist_Mono({
@@ -22,7 +26,31 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* header */}
+            <Header/>
+            <main className="min-h-screen">{children}</main>
+              {/* footer */}
+              <footer className="bg-muted/50 py-12">
+                <div className="container mx-auto text-center px-4 text-gray-200">
+                  <p>
+                    Created by{" "}
+                    <a
+                      href="https://github.com/iamshaunjp"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      @iamshaunjp
+                    </a>
+                  </p>
+                </div>
+              </footer>
+          </ThemeProvider>
       </body>
     </html>
   );
